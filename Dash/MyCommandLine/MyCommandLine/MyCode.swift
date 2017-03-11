af7a0cc27db8627c7018dbd991d0e366a5589594
chenwq <chenwq@retechcorp.com>
2017-03-11 15:20:29 +0800

    swift class with private

---
+++ Dash/MyCommandLine/MyCommandLine/MyCode.swift
@@ -0,0 +1,20 @@
+//
+//  MyCode.swift
+//  MyCommandLine
+//
+//  Created by chenwenqiang on 2017/3/11.
+//  Copyright © 2017年 cn.readingday. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    func  writeHello() {
+        print("Hello")
+    }
+    
+    private func writePassword() {
+        print("secret")
+    }
+
+}
---
+++ Dash/MyCommandLine/MyCommandLine/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  MyCommandLine
+//
+//  Created by chenwenqiang on 2017/3/11.
+//  Copyright © 2017年 cn.readingday. All rights reserved.
+//
+
+import Foundation
+
+
+let myClass = MyClass()
+    myClass.writeHello()
+
+
+
